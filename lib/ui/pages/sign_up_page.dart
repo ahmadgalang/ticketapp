0c5f7ca5af4dcdfbf47686560d4a2b09b86f4d4b
DESKTOP-VAL3T2M\Ahmad Galang <258galang@gmail.com>
2024-12-08 17:09:13 +0700

    slicing and changes page sign up

--- lib/ui/pages/sign_up_page.dart
+++ lib/ui/pages/sign_up_page.dart
@@ -13,198 +13,102 @@ class SignUpPage extends StatefulWidget {
 class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: kBackgroundColor,
-      body: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 30),
-        child: ListView(
-          children: [
-            Text(
-              'Join us and get \nyour next journey',
-              style:
-                  blackTextStyle.copyWith(fontSize: 24, fontWeight: semiBold),
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 37, bottom: 43),
-              width: double.infinity,
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-              decoration: BoxDecoration(
-                  color: kWhiteColor,
-                  borderRadius: BorderRadius.circular(defaultRadius)),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 20),
-                    width: double.infinity,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Full name',
-                          style: blackTextStyle.copyWith(
-                              fontSize: 14, fontWeight: normal),
-                        ),
-                        const SizedBox(height: 6),
-                        TextFormField(
-                          decoration: InputDecoration(
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color: kPrimaryColor,
-                                  width: 1), // Border saat fokus
-                              borderRadius:
-                                  BorderRadius.circular(defaultRadius),
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(
-                                  color:
-                                      Colors.grey), // Border saat tidak fokus
-                              borderRadius:
-                                  BorderRadius.circular(defaultRadius),
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 20),
-                    width: double.infinity,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Email Address',
-                          style: blackTextStyle.copyWith(
-                              fontSize: 14, fontWeight: normal),
-                        ),
-                        const SizedBox(height: 6),
-                        TextFormField(
-                          decoration: InputDecoration(
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color: kPrimaryColor,
-                                  width: 1), // Border saat fokus
-                              borderRadius:
-                                  BorderRadius.circular(defaultRadius),
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(
-                                  color:
-                                      Colors.grey), // Border saat tidak fokus
-                              borderRadius:
-                                  BorderRadius.circular(defaultRadius),
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 20),
-                    width: double.infinity,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Password',
-                          style: blackTextStyle.copyWith(
-                              fontSize: 14, fontWeight: normal),
-                        ),
-                        const SizedBox(height: 6),
-                        TextFormField(
-                          decoration: InputDecoration(
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color: kPrimaryColor,
-                                  width: 1), // Border saat fokus
-                              borderRadius:
-                                  BorderRadius.circular(defaultRadius),
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(
-                                  color:
-                                      Colors.grey), // Border saat tidak fokus
-                              borderRadius:
-                                  BorderRadius.circular(defaultRadius),
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 20),
-                    width: double.infinity,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Hobby',
-                          style: blackTextStyle.copyWith(
-                              fontSize: 14, fontWeight: normal),
-                        ),
-                        const SizedBox(height: 6),
-                        TextFormField(
-                          decoration: InputDecoration(
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color: kPrimaryColor,
-                                  width: 1), // Border saat fokus
-                              borderRadius:
-                                  BorderRadius.circular(defaultRadius),
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(
-                                  color:
-                                      Colors.grey), // Border saat tidak fokus
-                              borderRadius:
-                                  BorderRadius.circular(defaultRadius),
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 10),
-                    height: 55,
-                    width: double.infinity,
-                    child: TextButton(
-                      style: TextButton.styleFrom(
-                        backgroundColor: kPrimaryColor,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(defaultRadius)),
-                      ),
-                      onPressed: () {
-                        Navigator.pushNamed(context, '');
-                      },
-                      child: Text(
-                        'Submit',
-                        style: whiteTextStyle.copyWith(
-                          fontWeight: semiBold,
-                          fontSize: 18,
-                        ),
-                      ),
-                    ),
-                  )
-                ],
+    Widget title() {
+      return Container(
+        width: double.infinity,
+        child: Text(
+          'Join us and get \nyour next journey',
+          style: blackTextStyle.copyWith(fontSize: 24, fontWeight: semiBold),
+        ),
+      );
+    }
+
+    Widget inputSection() {
+      Widget field() {
+        return Container(
+          margin: const EdgeInsets.only(bottom: 20),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Full name',
+                style:
+                    blackTextStyle.copyWith(fontSize: 14, fontWeight: medium),
               ),
-            ),
-            Center(
-              child: GestureDetector(
-                onTap: () {
-                  if (kDebugMode) {
-                    print('object');
-                  }
-                },
-                child: Text(
-                  'Terms and Conditions',
-                  style: greyTextStyle.copyWith(fontSize: 14, fontWeight: normal, decoration: TextDecoration.underline),
+              const SizedBox(height: 6),
+              TextFormField(
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: kPrimaryColor, width: 2), // Border saat fokus
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                        color: Colors.grey), // Border saat tidak fokus
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                  ),
                 ),
+              )
+            ],
+          ),
+        );
+      }
+
+      Widget buttonSubmit() {
+        return Container(
+          margin: const EdgeInsets.only(top: 10, bottom: 10),
+          height: 55,
+          width: double.infinity,
+          child: TextButton(
+            style: TextButton.styleFrom(
+              backgroundColor: kPrimaryColor,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(defaultRadius)),
+            ),
+            onPressed: () {
+              Navigator.pushNamed(context, '');
+            },
+            child: Text(
+              'Get Started',
+              style: whiteTextStyle.copyWith(
+                fontWeight: semiBold,
+                fontSize: 18,
               ),
             ),
+          ),
+        );
+      }
+
+      return Container(
+        margin: const EdgeInsets.only(top: 37),
+        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: kWhiteColor,
+          borderRadius: BorderRadius.circular(defaultRadius),
+        ),
+        child: Column(
+          children: [
+            field(),
+            field(),
+            field(),
+            field(),
+            buttonSubmit()
           ],
+        ),
+      );
+    }
+
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: kBackgroundColor,
+        body: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 30),
+          child: ListView(
+            children: [title(), inputSection()],
+          ),
         ),
       ),
     );
