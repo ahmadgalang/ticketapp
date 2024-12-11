0d87d96ffa5e3112fae262e2d18714e5aad83066
DESKTOP-VAL3T2M\Ahmad Galang <258galang@gmail.com>
2024-12-11 20:16:52 +0700

    all

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,14 +1,45 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:ticketapp/shared/theme.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+
+  Widget header() {
+    return Container(
+      margin: const EdgeInsets.only(top: 30, left: 24, right: 24),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Ahmad,\nGalang Afianto', style: blackTextStyle.copyWith(fontSize: 18, fontWeight: bold),),
+                Text('Where to fly today?', style: greyTextStyle.copyWith(fontSize: 16, fontWeight: light),)
+              ],
+            ),
+          ),
+          SizedBox(height: 18),
+          Container(
+            height: 50,
+            width: 50,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage('lib/assets/image-profile.png'),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: [
-
+        header()
       ],
     );
   }
--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -1,206 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:ticketapp/shared/theme.dart';
+import 'package:ticketapp/ui/pages/home_page.dart';
 import 'package:ticketapp/ui/widgets/custom_bottom_navigation.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
-  Widget header() {
-    Widget title() {
-      return Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Howdy,\nKezia Anne',
-              style:
-                  blackTextStyle.copyWith(fontSize: 24, fontWeight: semiBold),
-            ),
-            Text(
-              'Where to fly today?',
-              style: greyTextStyle.copyWith(fontSize: 16, fontWeight: light),
-            ),
-          ],
-        ),
-      );
-    }
-
-    Widget profile() {
-      return Container(
-        width: 60,
-        height: 60,
-        decoration: const BoxDecoration(
-          shape: BoxShape.circle,
-          image: DecorationImage(
-            image: AssetImage('lib/assets/image-profile.png'),
-          ),
-        ),
-      );
-    }
-
-    return Container(
-      margin: const EdgeInsets.only(bottom: 30),
-      width: double.infinity,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [title(), profile()],
-      ),
-    );
+  Widget buildContent(){
+    return HomePage();
   }
 
-  Widget favoritDestionation() {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Row(
-        children: [
-          Container(
-            margin: const EdgeInsets.only(right: 24),
-            padding: const EdgeInsets.all(10),
-            height: 323,
-            width: 200,
-            decoration: BoxDecoration(
-              color: kWhiteColor,
-              borderRadius: BorderRadius.circular(defaultRadius),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  width: double.infinity,
-                  height: 220,
-                  decoration: BoxDecoration(
-                      image: const DecorationImage(
-                          image: AssetImage(
-                              'lib/assets/image-destination-tiga.png'),
-                          fit: BoxFit.cover),
-                      borderRadius: BorderRadius.circular(defaultRadius)),
-                  child: Align(
-                    alignment: Alignment.topRight,
-                    child: Container(
-                      height: 30,
-                      width: 54.5,
-                      decoration: BoxDecoration(
-                        color: kWhiteColor,
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(defaultRadius),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Lake Ciliwung',
-                        style: blackTextStyle.copyWith(
-                            fontSize: 18, fontWeight: medium),
-                      ),
-                      const SizedBox(height: 5),
-                      Text(
-                        'Tangerang',
-                        style: greyTextStyle.copyWith(
-                            fontSize: 14, fontWeight: light),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget popularDestination() {
-    return SingleChildScrollView(
-      scrollDirection: Axis.vertical,
+  Widget customButtonNavigation() {
+    return Align(
+      alignment: Alignment.bottomCenter,
       child: Container(
-        margin: const EdgeInsets.only(top: 515),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              padding: const EdgeInsets.all(10),
-              margin: const EdgeInsets.only(top: 16),
-              width: double.infinity,
-              height: 90,
-              decoration: BoxDecoration(
-                  color: kWhiteColor,
-                  borderRadius: BorderRadius.circular(defaultRadius)),
-              child: Row(
-                children: [
-                  Container(
-                    width: 70,
-                    height: 70,
-                    decoration: BoxDecoration(
-                        image: const DecorationImage(
-                            image: AssetImage(
-                                'lib/assets/image-destination-satu.png'),
-                            fit: BoxFit.cover),
-                        borderRadius: BorderRadius.circular(defaultRadius)),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              margin: const EdgeInsets.only(top: 16),
-              width: double.infinity,
-              height: 90,
-              decoration: BoxDecoration(
-                  color: kWhiteColor,
-                  borderRadius: BorderRadius.circular(defaultRadius)),
-              child: Row(
-                children: [
-                  Container(
-                    width: 70,
-                    height: 70,
-                    decoration: BoxDecoration(
-                        image: const DecorationImage(
-                            image: AssetImage(
-                                'lib/assets/image-destination-satu.png'),
-                            fit: BoxFit.cover),
-                        borderRadius: BorderRadius.circular(defaultRadius)),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              margin: const EdgeInsets.only(top: 16),
-              width: double.infinity,
-              height: 90,
-              decoration: BoxDecoration(
-                  color: kWhiteColor,
-                  borderRadius: BorderRadius.circular(defaultRadius)),
-              child: Row(
-                children: [
-                  Container(
-                    width: 70,
-                    height: 70,
-                    decoration: BoxDecoration(
-                        image: const DecorationImage(
-                            image: AssetImage(
-                                'lib/assets/image-destination-satu.png'),
-                            fit: BoxFit.cover),
-                        borderRadius: BorderRadius.circular(defaultRadius)),
-                  )
-                ],
-              ),
-            )
-          ],
+        width: double.infinity,
+        height: 60,
+        margin: EdgeInsets.only(
+            left: defaultMargin, right: defaultMargin, bottom: 30),
+        decoration: BoxDecoration(
+          color: kWhiteColor,
+          borderRadius: BorderRadius.circular(defaultRadius),
         ),
+        child:
+            const Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+              CustomButtonNavigation(imageUrl: 'lib/assets/icon-home.png',),
+              CustomButtonNavigation(imageUrl: 'lib/assets/icon-card.png',),
+              CustomButtonNavigation(imageUrl: 'lib/assets/icon-transaction.png',),
+              CustomButtonNavigation(imageUrl: 'lib/assets/icon-settings.png',),
+            ]),
       ),
     );
-  }
-
-  Widget customNavigation() {
-    return const CustomButtonNavigation();
   }
 
   @override
@@ -210,7 +40,8 @@ class MainPage extends StatelessWidget {
         backgroundColor: kBackgroundColor,
         body: Stack(
           children: [
-            customNavigation(),
+            buildContent(),
+            customButtonNavigation(),
           ],
         ),
       ),
